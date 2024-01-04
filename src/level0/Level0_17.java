46aeed34b699c5357ba329a76164bdb95f31aa86
parkjungsub <eses565@naver.com>
2024-01-04 09:56:27 +0900

    두수의 곱

---
+++ src/level0/Level0_17.java
@@ -0,0 +1,14 @@
+package level0;
+
+public class Level0_17 {
+    public static void main(String[] args) {
+        System.out.println(solution(1,2));
+    }
+    public static int solution(int num1, int num2) {
+        int answer = 0;
+        answer = num1 * num2;
+        return answer;
+    }
+}
+
+
