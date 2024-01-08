94519252e44fc56de1dc20382c317010c6c2fe18
parkjungsub <eses565@naver.com>
2024-01-08 09:31:39 +0900

    숫자 비교하기

---
+++ src/level0/Level0_20.java
@@ -0,0 +1,16 @@
+package level0;
+
+public class Level0_20 {
+    public static void main(String[] args) {
+        System.out.println(solution(2,3));
+    }
+    public static int solution(int num1, int num2) {
+        int answer = 0;
+        if(num1 == num2){
+            answer = 1;
+        }else {
+            answer = -1;
+        }
+        return answer;
+    }
+}
