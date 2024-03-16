8cc1b58967e4ac98d541686b743e814e91291739
parkjungsub <eses565@naver.com>
2024-03-17 00:25:40 +0900

    문자 반복 출력하기

--- src/level0/Level0_31.java
+++ src/level0/Level0_31.java
@@ -2,6 +2,7 @@ package level0;
 //문자열 my_string과 정수 n이 매개변수로 주어질 때,
 // my_string에 들어있는 각 문자를 n만큼 반복한 문자열을 return 하도록 solution 함수를 완성해보세요.
 public class Level0_31 { //문자 반복 출력하기
+
     public static void main(String[] args) {
         System.out.println(solution("hello",3));
     }
